17bbc264b79491cfe0a5f0c5ac93fbb1db835a7a
medusanxs <terminalfears@gmail.com>
2015-04-30 19:16:47 -0500

    Ruby

---
+++ 001.rb
@@ -0,0 +1,24 @@
+puts "Hello, Matz!"
+
+#!/usr/local/bin/ruby
+# a nice greeting for Matz
+puts "Hello, Matz!"
+
+system "echo 'Hello, Matz!'"
+
+system "echo", "Hello", "Matz!"
+
+puts "hello," + "matz!"
+
+puts "Hello," << "Matz!"
+
+puts "hello, matz!" * 2
+
+5.times { print "Hello, Matz! " }
+
+puts "Hello," * 3 + "Maz!"
+
+puts "Hey Matz, I'm running" + 'ruby --version'
+
+puts "Hello," + {print "Matz"}
+
